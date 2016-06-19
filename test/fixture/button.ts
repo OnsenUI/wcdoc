6816f658c064ffce79b696b02293a55c5d97f0a7
Mitsunori Kubota <anatoo.jp@gmail.com>
2016-06-20 06:54:21 +0900

    Updated fixture file for directives.

--- test/fixture/button.ts
+++ test/fixture/button.ts
@@ -1,16 +1,28 @@
 
 /**
- * @directive x-button
- * @selector button
+ * @directive my-button
+ * @selector button.my-button
  * @desc This is a directive for button element.
  */
 @Directive({
-  selector: 'button'
+  selector: 'button.my-button'
 })
 class Button {
 
   /**
-   * @attribute label
-   * @desc This is the description for label attribute.
+   * @input label
+   * @type {string}
+   * @desc This is the description for label input.
    */
+  @Input() get label(label: string) {
+    /* ... */
+  }
+
+  /**
+   * @output checked
+   * @type {boolean}
+   * @desc This is the description for checked output.
+   */
+  @Onput() checked: EventEmitter<boolean> = new EventEmitter();
+
 }
